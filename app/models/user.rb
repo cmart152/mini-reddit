a0f8b96d21282c69d0e86b717f102893f260872d
Chester Martin <chester.martin89@gmail.com>
2023-01-26 10:34:41 +0000

    Models and assosiations set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+
+  validates :name, presence: true, length:  { minimum: 3 }
+  validates :email, presence: true, format: { with: /[a-zA-Z\d]+@+[a-zA-Z\d]+\.+[a-zA-Z\d]+/}
+  validates :password, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_141300) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_100908) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
